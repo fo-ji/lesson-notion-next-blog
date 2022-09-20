68f0eea31a282c5a9d00c1cad07ce2258bed4a9c
fo-ji <fumiya1753@gmail.com>
2022-09-20 19:00:54 +0900

    add domain images

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.notion.so'],
+    domains: ['www.notion.so', 's3.us-west-2.amazonaws.com'],
   },
 }
 
